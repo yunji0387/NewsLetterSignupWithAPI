72b6c0c463c83a0b5593cddc21339a15b15d0302
yunji0387 <yunji0387@gmail.com>
2023-05-25 15:41:13 -0500

    adding initial files.

---
+++ app.js
@@ -0,0 +1,77 @@
+//jshint esversion: 6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    let data = {
+        "Name": firstName + " " + lastName,
+        "Email": email 
+    };
+
+    // console.log(firstName + ", " + lastName + ", " + email);
+    // console.log("----------");
+    // console.log(data);
+    let jsonData = JSON.stringify(data);
+    console.log("----------");
+    console.log(jsonData);
+
+    // const url = "https://api.mailjet.com/v3/REST/contact";
+
+    // const options = {
+    //     method: "POST",
+    //     auth: "daily:2c92688731400b760684e3760efb912b"
+    // };
+    // const request = https.request(url, options, function(response){
+    //     response.on("data", function(data){
+    //         console.log(JSON.parse(data));
+    //     });
+    // });
+
+    // request.write(jsonData);
+    // request.end();
+
+    const mailjet = require ('node-mailjet')
+        .connect(process.env.MJ_APIKEY_PUBLIC, process.env.MJ_APIKEY_PRIVATE)
+    const request = mailjet
+        .post("contact", {'version': 'v3'})
+        .request({
+        "IsExcludedFromCampaigns":"true",
+        "Name":firstName + " " + lastName,
+        "Email":email
+        })
+    request
+        .then((result) => {
+            console.log(result.body)
+        })
+        .catch((err) => {
+            console.log(err.statusCode)
+        })
+});
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000");
+});
+
+
+
+//api key
+// 2c92688731400b760684e3760efb912b
+
+//list ID
+//10315956
